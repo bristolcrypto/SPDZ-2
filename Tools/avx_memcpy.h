a80fd50deaceedf05e6f2556f0a11fc8a38797bf
Marcel Keller <m.keller@bristol.ac.uk>
2018-01-03 13:15:45 +0100

    Support for processors without AVX.

--- Tools/avx_memcpy.h
+++ Tools/avx_memcpy.h
@@ -14,11 +14,13 @@
 inline void avx_memcpy(void* dest, const void* source, size_t length)
 {
 	__m256i* d = (__m256i*)dest, *s = (__m256i*)source;
+#ifdef __AVX__
 	while (length >= 32)
 	{
 		_mm256_storeu_si256(d++, _mm256_loadu_si256(s++));
 		length -= 32;
 	}
+#endif
 	__m128i* d2 = (__m128i*)d;
 	__m128i* s2 = (__m128i*)s;
 	while (length >= 16)
@@ -33,12 +35,14 @@ inline void avx_memcpy(void* dest, const void* source, size_t length)
 inline void avx_memzero(void* dest, size_t length)
 {
 	__m256i* d = (__m256i*)dest;
+#ifdef __AVX__
 	__m256i s = _mm256_setzero_si256();
 	while (length >= 32)
 	{
 		_mm256_storeu_si256(d++, s);
 		length -= 32;
 	}
+#endif
 	if (length)
 		memset(d, 0, length);
 }
