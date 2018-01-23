0ba82d4b2ad90503ae2ad194e900c0b961db3936
Marcel Keller <m.keller@bristol.ac.uk>
2018-01-23 17:39:23 +0000

    Add zeros to match buffer size.

--- Scripts/gen_input_f2n.cpp
+++ Scripts/gen_input_f2n.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include "Math/gf2n.h"
+#include "Processor/Buffer.h"
 
 using namespace std;
 
@@ -18,6 +19,10 @@ int main() {
 		cerr << "value is: " << x << "\n";
 		x.output(cout,false);
 	}
+	n = -(n % BUFFER_SIZE) + BUFFER_SIZE;
+	cerr << "Adding " << n << " zeros to match buffer size" << endl;
+	for (int i = 0; i < n; i++)
+		gf2n(0).output(cout, false);
 
 	cin.close();
 	cout.close();
--- Scripts/gen_input_fp.cpp
+++ Scripts/gen_input_fp.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include "Math/gfp.h"
+#include "Processor/Buffer.h"
 
 using namespace std;
 
@@ -20,6 +21,10 @@ int main() {
 		to_gfp(b, a);
 		b.output(cout, false);
 	}
+	n = -(n % BUFFER_SIZE) + BUFFER_SIZE;
+	cerr << "Adding " << n << " zeros to match buffer size" << endl;
+	for (int i = 0; i < n; i++)
+		gfp(0).output(cout, false);
 
 	cin.close();	
 	cout.close();
