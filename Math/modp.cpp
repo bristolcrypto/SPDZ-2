1f5b7e885d8de788e035aedc85308c8244cf2a3d
Marcel Keller <mks.keller@gmail.com>
2018-07-05 20:08:22 +0200

    Insufficient randomizing in GF(p).

--- Math/modp.cpp
+++ Math/modp.cpp
@@ -14,7 +14,7 @@ bool modp::rewind = false;
 void modp::randomize(PRNG& G, const Zp_Data& ZpD)
 {
   bigint x=G.randomBnd(ZpD.pr);
-  memcpy(this->x, x.get_mpz_t()->_mp_d, ZpD.get_t());
+  memcpy(this->x, x.get_mpz_t()->_mp_d, ZpD.get_t() * sizeof(mp_limb_t));
 }
 
 void modp::pack(octetStream& o,const Zp_Data& ZpD) const
